f2631826ceba9c9d5f95eae6102442bfba061832
Saif <sshussaini96@gmail.com>
2021-07-07 17:42:07 -0400

    php fixes again

--- contact-handler.php
+++ contact-handler.php
@@ -9,7 +9,7 @@
 
     $to = "sshussaini96@gmail.com";
     $headers = "From:  $email_from \r\n";
-    $headers = "Reply-To: $visitor_email \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
     mail($to,$email_subject,$email_body,$headers);
     header("Location: index.html")
 ?>
