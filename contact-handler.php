7718cb63b8bb54ba0e6b6b8669c625831a4ce0f5
Saif <sshussaini96@gmail.com>
2021-07-07 17:39:35 -0400

    php fixes

--- contact-handler.php
+++ contact-handler.php
@@ -5,11 +5,11 @@
 
     $email_from = 'sshussaini96@gmail.com';
     $email_subject = "New Form Submission";
-    $email_body = "User Name: $name.\n", "User Email: $visitor_email.\n", "User Message: $message.\n";
+    $email_body = "User Name: $name.\n". "User Email: $visitor_email.\n". "User Message: $message.\n";
 
     $to = "sshussaini96@gmail.com";
     $headers = "From:  $email_from \r\n";
-    $headers .= "Reply-To: $visitor_email \r\n";
+    $headers = "Reply-To: $visitor_email \r\n";
     mail($to,$email_subject,$email_body,$headers);
     header("Location: index.html")
 ?>
